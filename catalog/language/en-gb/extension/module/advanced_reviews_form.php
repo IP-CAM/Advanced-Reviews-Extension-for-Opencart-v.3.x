52bc28f622f4f398d6e3856a32458146ebcd8223
Marvin Klein <klein@karley.eu>
2022-07-13 12:03:11 +0200

    Validate terms

--- catalog/language/en-gb/extension/module/advanced_reviews_form.php
+++ catalog/language/en-gb/extension/module/advanced_reviews_form.php
@@ -6,4 +6,5 @@ $_['text_agree']   = 'I have read and agree to the <a href="%s" class="agree"><b
 $_['entry_email']  = 'E-mail address';
 
 // Errors
-$_['error_email']  = 'E-mail must be between 5 and 96 characters!';;
+$_['error_email']  = 'E-mail must be between 5 and 96 characters!';
+$_['error_agree']  = 'Error: You must agree to the %s!';
