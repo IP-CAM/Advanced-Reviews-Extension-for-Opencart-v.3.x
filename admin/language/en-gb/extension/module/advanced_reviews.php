9f572866e86acda2035cc2349388131e95374387
Marvin Klein <klein@karley.eu>
2022-07-13 10:35:39 +0200

    Update English language

--- admin/language/en-gb/extension/module/advanced_reviews.php
+++ admin/language/en-gb/extension/module/advanced_reviews.php
@@ -1,14 +1,22 @@
 <?php
-// Heading
-$_['heading_title']    = 'Information';
+$_['heading_title']			= 'Advanced Reviews';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified information module!';
-$_['text_edit']        = 'Edit Information Module';
+$_['text_extension']		= 'Extensions';
+$_['text_success']			= 'Record successfully edited';
+$_['text_edit']				= 'Edit';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']			= 'Status';
+$_['entry_terms']			= 'Conditions reviews';
+$_['entry_cut_names']       = 'Cut off names';
+$_['entry_require_email']   = 'E-mail required';
+
+
+// Help
+$_['help_terms']		    = 'Must be agreed by the customer when submitting a rating';
+$_['help_cut_names']		= 'Customer names that are displayed in the product page are cut off in the second word after the first letter';
+$_['help_require_email']    = 'If enabled, the customer must provide their email address to rate';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify information module!';
+$_['error_permission']		= 'No permissions to edit';
---
+++ admin/language/en-gb/extension/module/advanced_reviews_form.php
@@ -0,0 +1,7 @@
+<?php
+// Entry
+$_['entry_verified']			= 'Verified';
+$_['entry_email']			    = 'E-mail address';
+
+// Error
+$_['error_email']		        = 'The email address must be between 0 and 96 characters long.';
