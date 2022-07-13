9e5e369ae819489e552b5e8aeba7e8fb3765c1ba
Marvin Klein <klein@karley.eu>
2022-07-13 11:57:17 +0200

    Validate e-mail field

--- catalog/language/en-gb/extension/module/advanced_reviews_form.php
+++ catalog/language/en-gb/extension/module/advanced_reviews_form.php
@@ -4,3 +4,6 @@ $_['text_agree']   = 'I have read and agree to the <a href="%s" class="agree"><b
 
 // Entries
 $_['entry_email']  = 'E-mail address';
+
+// Errors
+$_['error_email']  = 'E-mail must be between 5 and 96 characters!';;
