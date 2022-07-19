8e058b9746d6ec6384668d12be331c066b083353
Marvin Klein <klein@karley.eu>
2022-07-19 12:42:04 +0200

    Add English error messages for validation

--- src/admin/language/en-gb/extension/module/advanced_reviews.php
+++ src/admin/language/en-gb/extension/module/advanced_reviews.php
@@ -34,5 +34,6 @@ $_['tab_general']   = 'General';
 $_['tab_coupons']   = 'Coupons';
 
 // Error
-$_['error_permission']		    = 'No permissions to edit';
-$_['error_coupons_discount']    = 'You must specify a discount if you want to use coupons.';
+$_['error_permission']		            = 'No permissions to edit';
+$_['error_coupons_discount']            = 'You must specify a discount if you want to use coupons.';
+$_['error_coupons_email_required']      = '"E-Mail required" must be enabled if you want to use coupons.';
