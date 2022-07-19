f4664e160a05a65313e1408e69bd27d60c03e12b
Marvin Klein <klein@karley.eu>
2022-07-19 16:56:24 +0200

    Added language for admin email

--- src/admin/language/de-de/extension/module/advanced_reviews_form.php
+++ src/admin/language/de-de/extension/module/advanced_reviews_form.php
@@ -3,5 +3,9 @@
 $_['entry_verified']			= 'Verifiziert';
 $_['entry_email']			    = 'E-Mail Adresse';
 
+// Email
+$_['email_invalid_coupon_subject']      = 'Advanced Reviews - Ungültiger Gutscheincode';
+$_['email_invalid_coupon_text']         = 'Der Gutscheincode %s existiert nicht mehr oder ist abgelaufen. Bitte geben Sie einen neuen Gutscheincode in der Administration ein. Ansonsten werden Ihre Kunden keine automatischen E-Mails mehr mit Gutscheincodes erhalten, wenn eine Bewertung veröffentlicht wurde.';
+
 // Error
 $_['error_email']		        = 'Die E-Mail Adresse muss zwischen 0 und 96 Zeichen lang sein.';
--- src/admin/language/en-gb/extension/module/advanced_reviews_form.php
+++ src/admin/language/en-gb/extension/module/advanced_reviews_form.php
@@ -3,5 +3,9 @@
 $_['entry_verified']			= 'Verified';
 $_['entry_email']			    = 'E-mail address';
 
+// Email
+$_['email_invalid_coupon_subject']      = 'Advanced Reviews - Invalid Coupon code';
+$_['email_invalid_coupon_text']         = 'The coupon code %s does not exists anymore or has expired. Please set a new coupon code within the Administration. Otherwise your customers will not receive automated coupon emails when a review has been published.';
+
 // Error
 $_['error_email']		        = 'The email address must be between 0 and 96 characters long.';
