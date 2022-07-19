7abdfa00e577c2e7fabaed5c6971368655c391ea
Marvin Klein <klein@karley.eu>
2022-07-19 11:33:48 +0200

    Added German language for coupon feature

--- src/admin/language/de-de/extension/module/advanced_reviews.php
+++ src/admin/language/de-de/extension/module/advanced_reviews.php
@@ -7,18 +7,26 @@ $_['text_success']			= 'Datensatz erfolgreich bearbeitet';
 $_['text_edit']				= 'Bearbeiten';
 
 // Entry
-$_['entry_status']			= 'Status';
-$_['entry_terms']			= 'Bedingungen Bewertungen';
-$_['entry_verified_faq']	= 'Bewertungsinformationen';
-$_['entry_cut_names']       = 'Namen abschneiden';
-$_['entry_require_email']   = 'E-Mail benötigt';
+$_['entry_status']			    = 'Status';
+$_['entry_terms']			    = 'Bedingungen Bewertungen';
+$_['entry_verified_faq']	    = 'Bewertungsinformationen';
+$_['entry_cut_names']           = 'Namen abschneiden';
+$_['entry_require_email']       = 'E-Mail benötigt';
+$_['entry_enable_coupons']      = 'Gutscheine aktivieren';
+$_['entry_coupons_type']        = 'Art';
+$_['entry_coupons_discount']    = 'Nachlass';
+$_['entry_coupons_mailtext']    = 'Mailtext';
 
 
 // Help
-$_['help_terms']		    = 'Muss vom Kunden beim abgeben einer Bewertung zugestimmt werden';
-$_['help_verified_faq']		= 'Zeigt den ausgewählten Text auf der Bewertungsseite an. Dieseser Text soll dem Kunden darüber informieren, wie Bewertungen gesammelt und verifiziert werden (Pflicht für Shops in Europa)';
-$_['help_cut_names']		= 'Kundennamen die in der Produktseite ausgegeben werden, sind im zweiten Wort nach dem ersten Buchstaben abgeschnitten';
-$_['help_require_email']    = 'Wenn aktiviert, muss der Kunde zum Bewerten seine E-Mail Adresse angeben';
+$_['help_terms']		        = 'Muss vom Kunden beim abgeben einer Bewertung zugestimmt werden';
+$_['help_verified_faq']		    = 'Zeigt den ausgewählten Text auf der Bewertungsseite an. Dieseser Text soll dem Kunden darüber informieren, wie Bewertungen gesammelt und verifiziert werden (Pflicht für Shops in Europa)';
+$_['help_cut_names']		    = 'Kundennamen die in der Produktseite ausgegeben werden, sind im zweiten Wort nach dem ersten Buchstaben abgeschnitten';
+$_['help_require_email']        = 'Wenn aktiviert, muss der Kunde zum Bewerten seine E-Mail Adresse angeben';
+$_['help_enable_coupons']       = 'Wenn aktiviert, dann erhält der Kunde bei Freischaltung der Bewertung automatisch eine E-Mail mit einem Rabattcode für seine nächste Bestellung.';
+$_['help_coupons_type']         = 'Prozent oder Fixbetrag. Wird jeweils auf den Bruttobetrag gerechnet';
+$_['help_coupons_mailtext']     = 'Text der nach der Generierung des Coupons an den Kunden gesendet werden soll.';
 
 // Error
 $_['error_permission']		= 'Keine Rechte zum bearbeiten';
+$_['error_coupons_disount']	= 'Sie müssen einen Nachlass angeben, wenn Sie Gutscheine verwenden möchten.';
