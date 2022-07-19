fbbbd75e55d573034b9d3bd1ae6a18a6a20689d2
Marvin Klein <klein@karley.eu>
2022-07-19 11:37:23 +0200

    Added English language for coupons

--- src/admin/language/en-gb/extension/module/advanced_reviews.php
+++ src/admin/language/en-gb/extension/module/advanced_reviews.php
@@ -7,18 +7,26 @@ $_['text_success']			= 'Record successfully edited';
 $_['text_edit']				= 'Edit';
 
 // Entry
-$_['entry_status']			= 'Status';
-$_['entry_terms']			= 'Conditions reviews';
-$_['entry_verified_faq']	= 'Verifying information';
-$_['entry_cut_names']       = 'Cut off names';
-$_['entry_require_email']   = 'E-mail required';
+$_['entry_status']			    = 'Status';
+$_['entry_terms']			    = 'Conditions reviews';
+$_['entry_verified_faq']	    = 'Verifying information';
+$_['entry_cut_names']           = 'Cut off names';
+$_['entry_require_email']       = 'E-mail required';
+$_['entry_enable_coupons']      = 'Activate Vouchers';
+$_['entry_coupons_type']        = 'Type';
+$_['entry_coupons_discount']    = 'Discount';
+$_['entry_coupons_mailtext']    = 'Mailtext';
 
 
 // Help
-$_['help_terms']		    = 'Must be agreed by the customer when submitting a rating';
-$_['help_verified_faq']		= 'Shows the selected information as FAQ about the review verification process (required for Europe)';
-$_['help_cut_names']		= 'Customer names that are displayed in the product page are cut off in the second word after the first letter';
-$_['help_require_email']    = 'If enabled, the customer must provide their email address to rate';
+$_['help_terms']		        = 'Must be agreed by the customer when submitting a rating';
+$_['help_verified_faq']		    = 'Shows the selected information as FAQ about the review verification process (required for Europe)';
+$_['help_cut_names']		    = 'Customer names that are displayed in the product page are cut off in the second word after the first letter';
+$_['help_require_email']        = 'If enabled, the customer must provide their email address to rate';
+$_['help_enable_coupons']       = 'If activated, then the customer will automatically receive an email with a discount code for his next order when the review is approved.';
+$_['help_coupons_type']         = 'Percent or fixed amount. Calculated in each case on the gross amount';
+$_['help_coupons_mailtext']     = 'Text to be sent to the customer after generating the voucher.';
 
 // Error
 $_['error_permission']		= 'No permissions to edit';
+$_['error_coupons_disount'] = 'You must specify a discount if you want to use vouchers.';
